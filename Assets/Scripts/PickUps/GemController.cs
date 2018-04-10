88e83f650316e431e844ea6d8e0af3e5fe9322bf
George Sofianos <georgesofianosgr@gmail.com>
2018-04-10 14:31:25 +0300

    Added lifeTime to Gem, Also: * Replaced GemCollector Script with GemController

---
+++ Assets/Scripts/PickUps/GemController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemController : MonoBehaviour {
+
+	public float lifeTime = 5f;
+
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void FixedUpdate () {
+		timer += Time.deltaTime;
+
+		if (timer >= lifeTime) {
+			destroyGem ();
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.gameObject.CompareTag("Player")) {
+			GemManager gemManager = GameObject.Find ("GemManager").transform.GetComponent<GemManager> ();
+			gemManager.collect (gameObject);
+		}
+	}
+
+	void destroyGem() {
+		Destroy (transform.parent.gameObject);
+	}
+}
